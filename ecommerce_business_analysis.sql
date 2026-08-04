a2e04a7a92d495b96cdb711f7c22b54f1ad1213c
Angie <myloveyesu@gmail.com>
2026-08-03 23:10:49 -0400

    Add files via upload

---
+++ ecommerce_business_analysis.sql
@@ -0,0 +1,8 @@
+SELECT
+	p.product_cattegory_name,
+    SUM(OI.price) AS revenue
+FROM order_items oi
+JOIN products p
+	ON oi.product_id = p.product_id
+GROUP BY p.product_category_name
+ORDER BY revenue DESC;
